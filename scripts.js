7c7c538925a87d6d2be60e9a4ccd3890b49f2dc0
Fishy-King <84335603+Fishy-King@users.noreply.github.com>
2025-04-06 08:04:30 -0400

    Dark and simple mode
    Added 2 new themes (dark and simplified)

--- scripts.js
+++ scripts.js
@@ -85,3 +85,19 @@ function hideError() {
     imgElement4.src = pokemonSprite;
     imgElement4.style.display = "none";
 }
+
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initate() {
+    var style1 = document.getElementById("stylesheet1");
+    var style2 = document.getElementById("stylesheet2");
+    var style3 = document.getElementById("stylesheet3");
+
+    style1.onclick = function () { swapStyleSheet("default.css") };
+    style2.onclick = function () { swapStyleSheet("dark.css"); };
+    style3.onclick = function () { swapStyleSheet("simple.css"); };
+}
+
+window.onload = initate;
